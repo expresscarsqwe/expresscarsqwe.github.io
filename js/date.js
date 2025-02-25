fa4176b5b90fef7ead56e5d4a02f36f428bc2fc5
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-02-25 11:01:12 +0300

    Create date.js

---
+++ js/date.js
@@ -0,0 +1,4 @@
+// Получаем текущий год
+const currentYear = new Date().getFullYear();
+// Находим элемент с id "current-year" и устанавливаем его текстом текущий год
+document.getElementById('current-year').textContent = currentYear;
