bf137169fc2824295eea32304904b5aa37c60a78
ZoLiryzik <108295811+ZoLiryzik@users.noreply.github.com>
2025-03-16 18:25:45 +0300

    Update scripts.js

--- assets/js/pages/index/scripts.js
+++ assets/js/pages/index/scripts.js
@@ -142,7 +142,7 @@ services.forEach(service => {
             </figure>
             <div class='card__databox'>
             <h3 class='card-databox__heading'>${service.title}</h3>
-            <div class='card-databox__description'><p>${service.description}</p>  </div>
+            <div class='card-databox__description'><p>${service.description}</p><br><br> </div>
             <div class='card-databox__description'><h1>${service.price}</h1></div>
             <button class="button" onclick="openModal(${service.id})">Подробнее</button>
         </div>
